b0801d6f33bf1339fda56bd06d81e4a1aa6875bf
Jess Lê <jessicale90@gmail.com>
2023-08-01 15:15:36 -0600

    Adds Onboarding Flow AND TabBarFlow

--- CoordinatorPattern/Coordinator/ApplicationCoordinator.swift
+++ CoordinatorPattern/Coordinator/ApplicationCoordinator.swift
@@ -5,6 +5,7 @@
 //  Created by Jess Lê on 8/1/23.
 //
 
+import Combine
 import Foundation
 import SwiftUI
 import UIKit
@@ -13,20 +14,28 @@ class ApplicationCoordinator: Coordinatable {
 
     let window: UIWindow
     var childCoordinators = [Coordinatable]()
+    
+    let hasSeenOnBoarding = CurrentValueSubject<Bool, Never>(false)
+    var subscriptions = Set<AnyCancellable>()
 
     init(window: UIWindow) {
         self.window = window
     }
 
     func start() {
-//        let onboardingCoordinator = OnboardingCoordinator()
-//        onboardingCoordinator.start()
-//        self.childCoordinators = [onboardingCoordinator]
-//        window.rootViewController = onboardingCoordinator.rootViewController
-
-        let mainCoordinator = MainCoordinator()
-        mainCoordinator.start()
-        childCoordinators = [mainCoordinator]
-        window.rootViewController = mainCoordinator.rootViewController
+        hasSeenOnBoarding.sink { [weak self] hasSeen in
+            guard let self = self else { return }
+            if hasSeen {
+                let mainCoordinator = MainCoordinator()
+                mainCoordinator.start()
+                self.childCoordinators = [mainCoordinator]
+                self.window.rootViewController = mainCoordinator.rootViewController
+            } else {
+                let onboardingCoordinator = OnboardingCoordinator(hasSeenOnBoarding: self.hasSeenOnBoarding)
+                onboardingCoordinator.start()
+                self.childCoordinators = [onboardingCoordinator]
+                self.window.rootViewController = onboardingCoordinator.rootViewController
+            }
+        }.store(in: &subscriptions)
     }
 }
--- CoordinatorPattern/Onboarding/OnboardingCoordinator.swift
+++ CoordinatorPattern/Onboarding/OnboardingCoordinator.swift
@@ -5,16 +5,22 @@
 //  Created by Jess Lê on 8/1/23.
 //
 
+import Combine
 import Foundation
 import SwiftUI
 
 class OnboardingCoordinator: Coordinatable {
 
     var rootViewController = UIViewController()
+    var hasSeenOnboarding: CurrentValueSubject<Bool, Never>
+
+    init(hasSeenOnBoarding: CurrentValueSubject<Bool, Never>) {
+        self.hasSeenOnboarding = hasSeenOnBoarding
+    }
 
     func start() {
-        let view = OnboardingView {
-
+        let view = OnboardingView { [weak self] in
+            self?.hasSeenOnboarding.send(true)
         }
         rootViewController = UIHostingController(rootView: view)
     }
